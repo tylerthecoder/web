49cbd241db7ba18a3d4e85b6c6354ae7ee58e4e7
Tyler Tracy <tylertracy1999@gmail.com>
2023-06-03 16:53:40 -0500

    Update deploy script

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -13,5 +13,10 @@ yarn web build
 cp -r ./apps/web/public ./apps/web/standalone/public
 cp -r ./apps/web/.next/static ./apps/web/standalone/.next/static
 
+
+pm2 kill
 pm2 start pm2-ecosystem.config.js
 
+# Autostart process on boot
+pm2 startup systemd
+pm2 save
