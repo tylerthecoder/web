2ebd425d1946607d63da3845a86e0987966ea8fb
Tyler Tracy <tylertracy1999@gmail.com>
2023-06-03 16:55:58 -0500

    Update deploy script

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -9,7 +9,7 @@ export NODE_ENV=production
 
 yarn api build
 
-yarn web build
+NEXT_PUBLIC_API_URL=https://api.tylertracy.com yarn web build
 cp -r ./apps/web/public ./apps/web/standalone/public
 cp -r ./apps/web/.next/static ./apps/web/standalone/.next/static
 
