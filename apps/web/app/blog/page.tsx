19f6b9e8a0ca81de0d78f755b0ba5a9261eff197
Tyler Tracy <tylertracy1999@gmail.com>
2023-09-10 09:06:13 -0500

    Add blog return type

--- apps/web/app/blog/page.tsx
+++ apps/web/app/blog/page.tsx
@@ -6,4 +6,5 @@ export default async function Page() {
 	const blogs = await API.getAllBlogs();
     const randomBlog = blogs[Math.floor(Math.random() * blogs.length)];
     redirect(`/blog/${randomBlog.id}`);
+    return <div></div>
 }
