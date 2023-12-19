6f18cc130e2c645bc42e99fce56a5242dd16897b
Tyler Tracy <tylertracy1999@gmail.com>
2023-12-19 17:54:08 -0600

    Remove old blog server

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,26 +6,24 @@ import { MeModule } from './me/me.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HueService } from './services/hue.service';
 import { VibageModule } from './vibage/vibage.module';
-import { BlogModule } from './blog/blog.module';
 import configuration from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [configuration]
+      load: [configuration],
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>("DB_URI"),
+        uri: configService.get<string>('DB_URI'),
       }),
     }),
     MeModule,
     VibageModule,
-    BlogModule,
   ],
   controllers: [AppController],
   providers: [ConfigService, AppService, HueService],
 })
-export class AppModule { }
+export class AppModule {}
--- apps/api/src/blog/blogs.data.ts
+++
@@ -1,6 +0,0 @@
-export const BLOGS = [
-  'Thoughts/Perception is everything.md',
-  "Thoughts/People don't evolve, system do.md",
-  'blog/Safety tech vs capabilities tech.md',
-];
-
